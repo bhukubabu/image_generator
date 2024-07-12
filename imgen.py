91d535ccfec253f4195631ca529d36a8f1efae65
JAYA BHUKTA <143258170+JayaBhukta@users.noreply.github.com>
2024-07-12 20:17:39 +0530

    Add files via upload

---
+++ imgen.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from matplotlib import pyplot as plt
+import requests
+import io
+from PIL import Image
+import time
+
+API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
+api_token ='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'
+headers = {"Authorization": f"Bearer {api_token}"}
+
+
+st.title("AI Art🎨 Generator ✨")
+st.subheader('',divider='rainbow')
+st.subheader("Hello I am your AI art 👨‍🎨assistant. I will generate artworks for you😊 Let's try ............")
+
+with st.container(height= 180):
+	prompt = st.text_area('Type your prompt here')
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+image_bytes = query({
+	"inputs": prompt,
+	"parameters": {
+		"guidance_scale": 8.6,
+		"num_inference_steps":90,
+	}
+})
+images = Image.open(io.BytesIO(image_bytes))
+images.save('images.jpg')
+but1 = st.button("Generate")
+
+if but1:
+	if prompt=="" :
+		st.error('Please provide a valid prompt', icon= "🚨")
+	else:
+		with st.spinner("Please wait......."):
+			time.sleep(5)
+		st.success('Done!')
+		st.image(images)
+		with open ('images.jpg','rb') as img:
+			but2 = st.download_button(
+			label="Download",
+			data=img,
+			file_name='images.jpg',
+			mime='image/jpg'
+			)
